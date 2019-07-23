0b53e16199093bbf38e3c5a12716bbb257d052bd
David Leon <leoc@unicauca.edu.co>
2019-07-23 02:14:52 -0500

    Initial Commit

---
+++ CopaAmericav2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CopaAmericav2
+//
+//  Created by Herman on 7/23/19.
+//  Copyright © 2019 Apimovil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
