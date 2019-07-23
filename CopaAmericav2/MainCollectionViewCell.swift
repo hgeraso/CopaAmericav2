cd1bdba95407ed68600a3b0bb278820b250cd370
David Leon <leoc@unicauca.edu.co>
2019-07-23 11:38:59 -0500

    No funciona.

---
+++ CopaAmericav2/MainCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MainCollectionViewCell.swift
+//  CopaAmericav2
+//
+//  Created by Herman on 7/23/19.
+//  Copyright © 2019 Apimovil. All rights reserved.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var team1: UILabel!
+    @IBOutlet var team2: UILabel!
+    @IBOutlet var score: UILabel!
+    @IBOutlet var dateMatch: UILabel!
+    
+    @IBOutlet weak var imgTeam1: UIImageView!
+    @IBOutlet weak var imgTeam2: UIImageView!
+}
--- CopaAmericav2/MainViewController.swift
+++ CopaAmericav2/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  CopaAmericav2
 //
 //  Created by Herman on 7/23/19.
@@ -9,18 +9,19 @@
 import UIKit
 
 class MainViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate,URLSessionDelegate {
-
-  
+    
+   
+    @IBOutlet var CollectionViewMain: UICollectionView!
     let jsonUrlString = "https://api.myjson.com/bins/gaenp"
     
-    var partidosServices: PartidoServiceProtocol = PartidoService()
+    var partidosServices:PartidoServiceProtocol = PartidoService()
     lazy var partidos:[Partido] = []
-    
-     override func viewDidLoad() {
-     super.viewDidLoad()
-     floatButtonScanner.layer.cornerRadius = floatButtonScanner.frame.height/2
-     loadPartidos()
-     }
+    /*
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        floatButtonScanner.layer.cornerRadius = floatButtonScanner.frame.height/2
+        loadPartidos()
+    }*/
     
     func loadPartidos() {
         partidosServices.getPartido(with: jsonUrlString) {
@@ -29,8 +30,8 @@ class MainViewController: UIViewController,UICollectionViewDataSource, UICollect
                 print("error")
                 return
             }else if let listPartidos = listPartidos{
-                print("=======================================")
-                print(listPartidos)
+                //print("=======================================")
+                //print(listPartidos)
                 self.partidos = listPartidos
                 self.agregarImg()
             }
@@ -39,7 +40,7 @@ class MainViewController: UIViewController,UICollectionViewDataSource, UICollect
     
     func agregarImg() {
         for index in 0...(partidos.count-1){
-            partidosServices.getImagePartido(with: partidos[index].urlFlagTeam1!) {
+            partidosServices.getImagePartido(with: partidos[index].urlFlagTeam1!)  {
                 (img, error) in
                 if error != nil { // Deal with error here
                     print("error")
@@ -70,16 +71,23 @@ class MainViewController: UIViewController,UICollectionViewDataSource, UICollect
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = CollectionViewMain.dequeueReusableCell(withReuseIdentifier: "celdaMain", for: indexPath) as! MainCollectionViewCell
+        let cell = CollectionViewMain.dequeueReusableCell(withReuseIdentifier: "CeldaMain", for: indexPath) as! MainCollectionViewCell
         cell.team1.text = partidos[indexPath.item].team1
         cell.team2.text = partidos[indexPath.item].team2
         cell.score.text = partidos[indexPath.item].score
-        cell.dateMatch.text = partidos[indexPath.item].dateMatch
         cell.imgTeam1.image = partidos[indexPath.item].flagTeam1
         cell.imgTeam2.image = partidos[indexPath.item].flagTeam2
         print("para cada imagen",partidos[indexPath.item].flagTeam1)
         return cell
     }
-     
+    
+    
+    
+    
+    
+    
 }
+
+
+
 
